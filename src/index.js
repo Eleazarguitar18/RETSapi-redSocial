fa902a0ab03eec1f9e924261a58c47ac3d2ffc37
Eleazarguitar18 <eleazarguitar18@gmail.com>
2023-11-30 13:17:13 -0400

    server

---
+++ src/index.js
@@ -0,0 +1,14 @@
+// app.js
+const express = require('express');
+const app = express();
+const port = 3000;
+
+// Ruta principal
+app.get('/', (req, res) => {
+    res.send('¡Hola, mundo!');
+});
+
+// Inicia el servidor
+app.listen(port, () => {
+    console.log(`Servidor escuchando en http://localhost:${port}`);
+});
