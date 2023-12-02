b8417c30aaf75065714649c15291285a0ae79a13
Eleazarguitar18 <eleazarguitar18@gmail.com>
2023-12-02 10:08:25 -0400

    modificando bd para usar squalize

--- src/routes/index.js
+++ src/routes/index.js
@@ -13,5 +13,14 @@ router.get('/usuarios', async (req, res) => {
         res.status(500).send('Error interno del servidor');
     }
 });
-
+// TODO: metodo para agregar datos , pendiente
+router.post('/usuarios', async (req, res) => {
+    try {
+        const [rows] = await db.query('SELECT * FROM Usuario');
+        res.json(rows);
+    } catch (error) {
+        console.error('Error al consultar la base de datos:', error);
+        res.status(500).send('Error interno del servidor');
+    }
+});
 module.exports = router;
